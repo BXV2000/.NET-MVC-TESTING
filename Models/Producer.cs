1d03150c2f33d0d77b2178d44cc087e6fab85bfe
BXV2000 <82429306+BXV2000@users.noreply.github.com>
2022-06-15 09:39:50 +0700

    add Cinema model

--- Models/Producer.cs
+++ Models/Producer.cs
@@ -5,7 +5,7 @@ namespace Test_Project.Models
     public class Producer
     {
         [Key]
-        public int ActorId { get; set; }
+        public int ProducerId { get; set; }
         public string ProfilePictureURL { get; set; }
         public string FullName { get; set; }
         public string Bio { get; set; }
